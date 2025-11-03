8a1dd92031c0a662a110755f66f97985d2191017
miguelgoncalves2024 <miguelgoncalves2024@hotmail.com>
2025-11-03 11:43:29 +0000

    Implementação bśica do Firebase Authentication

--- src/apis/firebase.ts
+++ src/apis/firebase.ts
@@ -4,11 +4,11 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA3L2aU485ibAgSWgmQRrhmwRTfYubvRDc",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
+  authDomain: "larica-b3364.firebaseapp.com",
   projectId: "larica-b3364",
-  storageBucket: "SEU_PROJETO.appspot.com",
+  storageBucket: "larica-b3364.appspot.com",
   messagingSenderId: "58329ed9b7dc294cc5fd74cf91d450055ffec4c5",
-  appId: "SEU_APP_ID"
+  appId: "213922994451"
 };
 
 // Inicializa Firebase
