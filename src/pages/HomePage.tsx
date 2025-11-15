4705d087bddee5e5a8c87176dcb6f179e9595ca1
miguelgoncalves2024 <miguelgoncalves2024@hotmail.com>
2025-11-15 12:33:01 +0000

    Pequeno header de merda

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Header from "../components/Header";
 
 export default function HomePage() {
@@ -9,13 +8,5 @@ export default function HomePage() {
         <h1 className="text-4xl text-light-one">Bem-vindo à Home Page!</h1>
       </main>
     </>
-=======
-export default function HomePage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-semibold">Home</h1>
-      <p>Bem-vindo!</p>
-    </div>
->>>>>>> 2055e7e (Atualização de Dependências)
   );
 }
